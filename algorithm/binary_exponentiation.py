7de319e6c507c39fdac818b8a7a192a8a1826234
noe <arkadiusz.nowak1983@gmail.com>
2020-07-07 13:53:56 +0200

    binary exponentiation: recursive vs linear solution

---
+++ algorithm/binary_exponentiation.py
@@ -0,0 +1,35 @@
+class BinaryExponentiation:
+    def __init__(self, base=1, exp=2):
+        self.base = 1 if base is None else base
+        self.exp = 2 if exp is None else exp
+
+    def calc(self, type='recurent'):
+        if type=='recurent':
+            return self.calc_recurent()
+        return self.calc_linear()
+
+    def calc_recurent(self):
+        '''
+        time complexity is O(log(n)) but recursive function will extend the execution time
+        :return: base multiplication by exp in recursive mode
+        '''
+        if self.exp == 0:
+            return 1
+        duplicated = BinaryExponentiation( self.base, int(self.exp / 2) ).calc()
+        duplicated = duplicated * duplicated
+        if self.exp % 2 == 1:
+            duplicated = duplicated * self.base
+        return duplicated
+
+    def calc_linear(self):
+        '''
+        time complexity is O(log(n)) and the linear algorithm is better solution
+        :return: base multiplication by exp in recursive mode
+        '''
+        result = 1
+        while(self.exp):
+            if self.exp % 2 == 1:
+                result = result * self.base
+            self.base = self.base * self.base
+            self.exp = int(self.exp / 2)
+        return result
---
+++ algorithm/test_binary_exponentiation.py
@@ -0,0 +1,20 @@
+import unittest
+from binary_exponentiation import  BinaryExponentiation
+
+class BinaryExponentiationTestCase(unittest.TestCase):
+    def test_recurent_pow_5_2(self):
+        self.assertEqual( BinaryExponentiation( 5 ).calc(), 25 )
+        self.assertEqual( BinaryExponentiation( 5, 2 ).calc(), 25 )
+    def test_recurent__7_13(self):
+        self.assertEqual( BinaryExponentiation( 7, 9 ).calc(), 40353607 )
+
+    def test_linear_pow_5_2(self):
+        self.assertEqual( BinaryExponentiation( 5 ).calc(), 25 )
+        self.assertEqual( BinaryExponentiation( 5, 2 ).calc(), 25 )
+    def test_linear__7_13(self):
+        self.assertEqual( BinaryExponentiation( 7, 9 ).calc(), 40353607 )
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(BinaryExponentiationTestCase)
+    #suite = unittest.TestLoader().loadTestsFromName('test_linear__7_13', BinaryExponentiationTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
