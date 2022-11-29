02412be34e7389f522f84ad5309d1a5e5073735c
arkadiusznowak1983 <arkadiusz.nowak1983@gmail.com>
2022-11-29 08:34:11 +0100

    Simple float type without precision problems

--- patterns/Money.py
+++ patterns/Money.py
@@ -1,5 +1,6 @@
+# Prevent float incermentation problems like, 0.6 + 0.7 = 1.2999999999999998
+#
 class Money(float):
-    # Prevent float incermentation problems like, 0.6 + 0.7 = 1.2999999999999998
     precision = 4
     multiplication = 10**precision
 
@@ -26,4 +27,13 @@ class Money(float):
 
 print('Incorrect ', 0.6+0.7)
 print('Fixed', Money(0.6) + Money(0.7))
+
+
+#  simplest solution
+class Float(float):
+    def __add__(self, other):
+        return float((int(float(self) * 10**4) + int(float(other) * 10**4)) / 10**4)
+
+
+print('Simple float __add__ overload', Float(0.6) + Float(0.7))
 
